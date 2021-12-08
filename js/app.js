419b9aa54517086d15ac8302c846623fcdfa08c7
TaielNxz <TaielNxz@hotmail.com>
2021-12-08 08:08:21 -0300

    mostrar en consola un array que contiene los cursos, sin duplicados

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@
 const carrito = document.querySelector("#carrito"); /*1*/
 const listaCursos = document.querySelector("#lista-cursos"); /*2*/
 
+// para Guardar articulos del carrito
+let articulosCarrito = []
+
 /* =========================================
                EventListeners
 ========================================= */
@@ -44,6 +47,32 @@ function extraerInfoCurso(curso) {
         cantidad : 1
     }
 
-    // Mostrar objeto en consola
-    console.log(infoCurso);
+    // Revisar si un elemento ya existe en el carrito
+    const existe = articulosCarrito.some( curso => curso.id === infoCurso.id )
+    
+    // Si existe... Actualizar Cantidad
+    if( existe ) {
+        // Crear nuevo arreglo con .map
+        const cursos = articulosCarrito.map( curso => {
+            if( curso.id === infoCurso.id ) {       
+                curso.cantidad++;
+                // retorna el objeto actualizado
+                return curso;  
+            } else {
+                // retorna los objetos que no son duplicados
+                return curso;
+            }
+        })
+        // Actualizar el arreglo de carrito
+        articulosCarrito = [ ...cursos ]
+
+        // Mostrar arreglo en consola
+        console.log(articulosCarrito)
+    } else {
+        // Agregar elemento al arreglo de carrito
+        articulosCarrito = [ ...articulosCarrito, infoCurso ];
+
+        // Mostrar arreglo en consola
+        console.log(articulosCarrito)
+    }
 }
